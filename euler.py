dc315a9620fa7c25e8329a12beffd0d12724b81f
Franciszek Podborski [pr15fp] <pr15fp@leeds.ac.uk>
2017-11-23 15:29:17 +0000

    fixed lambda function defining, exploring initial conditions for ODE

--- euler.py
+++ euler.py
@@ -1,11 +1,14 @@
 # Standard Euler method
 def euler(f, y_prev, h, n):
-    for step in range(1, n + 1):
-        y_next = y_prev + f(y_prev)
+    for step in range(n):
+        y_next = y_prev + h * f(y_prev)
         y_prev = y_next
     print(y_next)
+    return None
 
+# define the function f(y) as lambda y
+f = lambda y: 5*y - 3
 
-# need to look into defining f(y) for the euler() function
+euler(f, 1.0, 0.25, 4)
 
-euler(f, 1, 0.25, 4)
+# what are the initial conditions for this kind of equation?
