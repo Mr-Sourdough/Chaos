77cf51174d5b69fe404216e83b057c5596538d92
Franciszek Podborski [pr15fp] <pr15fp@leeds.ac.uk>
2017-11-23 16:54:56 +0000

    4th order Runge-Kutta method added and reordered the file

--- euler.py
+++ euler.py
@@ -1,3 +1,5 @@
+# The functions assume that initial condition (IC) is y(0) = y_prev
+
 # Standard Euler method
 def euler(f, y_prev, interval, n):
     h = interval / n  # define step size
@@ -7,9 +9,7 @@ def euler(f, y_prev, interval, n):
     print(y_next)
     return None
 
-# this function assumes that initial condition (IC) is y(0) = y_prev
-euler(lambda y: y, 1.0, 1, 8)
-
+# Modified Euler method
 def modEuler(f, y_prev, interval, n):
     h = interval / n  # define step size
     for step in range(n):
@@ -20,4 +20,19 @@ def modEuler(f, y_prev, interval, n):
     print(y_next)
     return None
 
-modEuler(lambda y: y, 1.0, 1, 8)
+# 4th order Runge-Kutta mathod
+def rk4(f, y_prev, interval, n):
+    h = interval / n  # define step size
+    for step in range(n):
+        k_1 = h * f(y_prev)
+        k_2 = h * f(y_prev + 0.5 * k_1)
+        k_3 = h * f(y_prev + 0.5 * k_2)
+        k_4 = h * f(y_prev + k_3)
+        y_next = y_prev + (k_1 + 2 * k_2 + 2 * k_3 + k_4)/6
+        y_prev = y_next  # reassign previous estimation of y with next
+    print(y_next)
+    return None
+
+euler(lambda y: y, 1.0, 1, 20)
+modEuler(lambda y: y, 1.0, 1, 20)
+rk4(lambda y: y, 1.0, 1, 20)
