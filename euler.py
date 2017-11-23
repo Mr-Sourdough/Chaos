9cb3e2ffad7811b8c505bf33c5f677194708c716
Franciszek Podborski [pr15fp] <pr15fp@leeds.ac.uk>
2017-11-23 16:44:52 +0000

    new file with timer function

--- euler.py
+++ euler.py
@@ -1,14 +1,23 @@
 # Standard Euler method
-def euler(f, y_prev, h, n):
+def euler(f, y_prev, interval, n):
+    h = interval / n  # define step size
     for step in range(n):
         y_next = y_prev + h * f(y_prev)
-        y_prev = y_next
+        y_prev = y_next  # reassign previous estimation of y with next
     print(y_next)
     return None
 
-# define the function f(y) as lambda y
-f = lambda y: 5*y - 3
+# this function assumes that initial condition (IC) is y(0) = y_prev
+euler(lambda y: y, 1.0, 1, 8)
 
-euler(f, 1.0, 0.25, 4)
+def modEuler(f, y_prev, interval, n):
+    h = interval / n  # define step size
+    for step in range(n):
+        k_1 = h * f(y_prev)
+        k_2 = h * f(y_prev + k_1)
+        y_next = y_prev + 0.5 * (k_1 + k_2)
+        y_prev = y_next  # reassign previous estimation of y with next
+    print(y_next)
+    return None
 
-# what are the initial conditions for this kind of equation?
+modEuler(lambda y: y, 1.0, 1, 8)
