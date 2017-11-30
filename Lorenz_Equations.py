ac098cc16f52ada62cdff9de503c331daf5cf3dd
Franciszek Podborski [pr15fp] <pr15fp@leeds.ac.uk>
2017-11-30 12:12:00 +0000

    Create callable definition

--- Lorenz_Equations.py
+++ Lorenz_Equations.py
@@ -1,52 +1,49 @@
-# time steps the Lorenz equations
 import numpy as np
 import matplotlib.pyplot as pt
 import timestepping as ts
 from mpl_toolkits.mplot3d import Axes3D
 
-''' modify to make into a function '''
+'''
+For this function, b, r, and sigma are the parameters in the Lorenz Equations
+initial time t is set to zero and runs to tmax
+with nsteps being the number of steps taken between t and tmax
+points is a list of tuples, where the first argument of the tuple
+is the position at the initial time in the form np.array([x, y, z])
+with x, y and z being the coordinates
+and the second being the colour of the line drawn from that point.
+If
+'''
 
-# parameters
-b = 8 / 3
-r = 28
-sigma = 10
+def lorenz_eq(b, r, sigma, t=0, tmax, nsteps, points, save=False):
+    # create 3d figure to plot on
+    fig = pt.figure()
+    ax = fig.gca(projection='3d')
 
-# plot the three variables in 3D
-fig = pt.figure()
-ax = fig.gca(projection='3d')
+    # calculate the time step
+    dt = tmax / nsteps
 
-# time-stepping parameters
-tmax = 2.0  # run to this time
-nsteps = 100  # number of time steps
-dt = tmax / nsteps  # calculate the time step
+    # first loop goes over each point
+    for x, c in points:
+        # initialise xs, ys and zs arrays
+        xs = np.array(x[0])
+        ys = np.array(x[1])
+        zs = np.array(x[2])
 
-# initial conditions
-t = 0
-x1 = np.array([1, 0, 0])
-x2 = np.array([0, -1, r - 1])
-points = [(x1, 'r'), (x2, 'b')]
+        n = 0  # number of timesteps taken; initialise to 0
 
-# first loop goes over each point
-for x, c in points:
-    # initialise x, y and z arrays
-    xs = np.array(x[0])
-    ys = np.array(x[1])
-    zs = np.array(x[2])
+        # creates the plot arrays
+        for steps in range(nsteps):
+            x = ts.step_rk2(x, dt, b, r, sigma)
+            t += dt
+            n += 1
+            xs = np.append(xs, x[0])
+            ys = np.append(ys, x[1])
+            zs = np.append(zs, x[2])
 
-    n = 0  # number of timesteps taken; initialise to 0
+            ax.plot(xs, ys, zs, c, lw=0.1)  # plot the arrays...
+        ax.scatter(x[0], x[1], x[2])  # and initial points
 
-    # creates the plot arrays
-    for steps in range(nsteps):
-        x = ts.step_rk2(x, dt, b, r, sigma)
-        t += dt
-        n += 1
-        xs = np.append(xs, x[0])
-        ys = np.append(ys, x[1])
-        zs = np.append(zs, x[2])
-
-        # plot the arrays and initial points
-        ax.plot(xs, ys, zs, c, lw=0.1)
-    ax.scatter(x[0], x[1], x[2])
-
-# show the final image
-pt.show()
+    if save == True:
+        pt.savefig()  # save plot to file...
+    else:
+        pt.show()  # or show the final image
