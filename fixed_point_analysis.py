65147acce10837f5004d4eec2a3e42455c7b9bb4
Franciszek Podborski [pr15fp] <pr15fp@leeds.ac.uk>
2017-12-02 16:59:34 +0000

    Further development to plot distance with time

--- fixed_point_analysis.py
+++ fixed_point_analysis.py
@@ -1,19 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import timestepping as ts
 
 x0 = np.array([10**-6, 10**-6, 10**-6])
 
 # This version of the lorenz_eq function generates the points
 # to measure the distance from x0
-def lorenz_points(b, r, sigma, tmax, nsteps, x, t=0, save=False):
 
+
+def lorenz_points(b, r, sigma, tmax, nsteps, x0, x, t=0, save=False):
     # calculate the time step
     dt = tmax / nsteps
 
     # creates the plot arrays
+    dist = np.empty()
     for steps in range(nsteps):
         x = ts.step_rk2(x, dt, b, r, sigma)
-        '''
-        add list to append the points to to be able to measure their distance
-        use np.linalg.norm(x - x0) to measure distance of x from x0
-        '''
+        np.append(dist, np.linalg.norm(x - x0))
+    # generate a time values array to plot dist against
+    ts = np.array([])
+    plt.plot(ts, dist)
