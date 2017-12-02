3f08a2c80640d0e1ededd05959fa45073c1364d6
Franciszek Podborski [pr15fp] <pr15fp@leeds.ac.uk>
2017-12-02 17:41:23 +0000

    dubuggin colour choice

--- Lorenz_Equations.py
+++ Lorenz_Equations.py
@@ -7,13 +7,16 @@ from mpl_toolkits.mplot3d import Axes3D
 For this function, b, r, and sigma are the parameters in the Lorenz Equations
 initial time t is set to zero and runs to tmax (needs to be a float)
 with nsteps being the number of steps taken between t and tmax
-points is a list initial points at initial t in the form np.array([x, y, z])
+points is a list of initial points at initial t in the form np.array([x, y, z])
 with x, y and z being the coordinates
 If save set to True, save the final plot as "Lorenz plot.png"
 '''
 
 
 def lorenz_eq(b, r, sigma, tmax, nsteps, points, t=0, save=False):
+    # colours
+    colours = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+
     # create 3d figure to plot on
     fig = plt.figure()
     ax = fig.gca(projection='3d')
@@ -22,7 +25,7 @@ def lorenz_eq(b, r, sigma, tmax, nsteps, points, t=0, save=False):
     dt = tmax / nsteps
 
     # first loop goes over each point
-    for x, colour in enumerate(points):
+    for colour, x in enumerate(points):
 
         # initialise xs, ys and zs arrays
         xs = np.array(x[0])
@@ -36,10 +39,12 @@ def lorenz_eq(b, r, sigma, tmax, nsteps, points, t=0, save=False):
             ys = np.append(ys, x[1])
             zs = np.append(zs, x[2])
 
-            ax.plot(xs, ys, zs, 'C{}'.format(colour), lw=0.1)  # plot the arrays...
+            ax.plot(xs, ys, zs, colours[colour], lw=0.1)  # plot the arrays...
         ax.scatter(x[0], x[1], x[2])  # and initial points
 
     if save is True:
         plt.savefig('Lorenz plot.png')  # save plot to file...
     else:
         plt.show()  # or show the final image
+
+lorenz_eq(8/3, 28, 10, 20.0, 500, [np.array([0, 1, 2])])
