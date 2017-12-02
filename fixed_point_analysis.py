a33745b5fd6cd4034e5fc3cb0b88fe6145ea0dd3
Franciszek Podborski [pr15fp] <pr15fp@leeds.ac.uk>
2017-12-02 18:15:11 +0000

    debugging

--- fixed_point_analysis.py
+++ fixed_point_analysis.py
@@ -7,6 +7,7 @@ x0 = np.array([10**-6, 10**-6, 10**-6])
 
 # This version of the lorenz_eq function generates the points
 # to measure the distance from x0
+# x is a single point as np.array
 
 
 def lorenz_points(b, r, sigma, tmax, nsteps, x):
@@ -18,13 +19,15 @@ def lorenz_points(b, r, sigma, tmax, nsteps, x):
     for steps in range(nsteps):
         x = tstep.step_rk2(x, dt, b, r, sigma)
         np.append(dist, np.linalg.norm(x - x0))
+
     # generate a time values array to plot dist against
     ts = np.array([n * dt for n in range(1, nsteps + 1)])
+
     plt.figure()
     plt.plot(ts, dist)
     plt.show()
+    return None
 
-print(type(x0))
-'''
-lorenz_points(8 / 3, 28, 10, 10.0, 500, [x0])
-lorenz_eq(8 / 3, 28, 10, 10.0, 500, [x0])'''
+
+lorenz_eq(8 / 3, 28, 10, 15.0, 750, [x0], save=True)
+lorenz_points(8 / 3, 28, 10, 15.0, 750, x0)
