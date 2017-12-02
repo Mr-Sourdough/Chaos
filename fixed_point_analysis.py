e88983568c792531ef6a04674369768710ddf2e5
Franciszek Podborski [pr15fp] <pr15fp@leeds.ac.uk>
2017-12-02 17:05:08 +0000

    debugging

--- fixed_point_analysis.py
+++ fixed_point_analysis.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import timestepping as ts
+import timestepping as tstep
 
 x0 = np.array([10**-6, 10**-6, 10**-6])
 
@@ -8,15 +8,18 @@ x0 = np.array([10**-6, 10**-6, 10**-6])
 # to measure the distance from x0
 
 
-def lorenz_points(b, r, sigma, tmax, nsteps, x0, x, t=0, save=False):
+def lorenz_points(b, r, sigma, tmax, nsteps, x):
     # calculate the time step
     dt = tmax / nsteps
 
     # creates the plot arrays
-    dist = np.empty()
+    dist = np.empty(nsteps)
     for steps in range(nsteps):
-        x = ts.step_rk2(x, dt, b, r, sigma)
+        x = tstep.step_rk2(x, dt, b, r, sigma)
         np.append(dist, np.linalg.norm(x - x0))
     # generate a time values array to plot dist against
-    ts = np.array([(t + n * dt) for n in range(1, nsteps + 1)])
+    ts = np.array([n * dt for n in range(1, nsteps + 1)])
     plt.plot(ts, dist)
+    plt.show()
+
+lorenz_points(8/3, 28, 10, 5.0, 200, x0)
