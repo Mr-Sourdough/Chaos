853ffe28610b1c038a25dab5077957b83c2e7d18
Frankie <f.podborski@gmail.com>
2018-02-04 16:00:03 +0000

    continue from previous commit

--- fixed_point_analysis.py
+++ fixed_point_analysis.py
@@ -3,28 +3,26 @@ import numpy as np
 import timestepping as tstep
 from Lorenz_Equations import lorenz_eq
 
-
+# initial point
+x0 = np.array([10**-6, 10**-6, 10**-6])
 
 # This version of the lorenz_eq function generates the points
 # to measure the distance from x0
 # x is a single point as np.array
 
 
-def lorenz_points(b, r, sigma, tmax, nsteps, x):  # put dt size instead of nsteps
+def lorenz_points(b, r, sigma, dt, nsteps, x):
     # initial point
-    x0 = np.array([10**-6, 10**-6, 10**-6])
-
-    # calculate the time step
-    dt = tmax / nsteps
+    x_init = x
 
     # creates the plot arrays
     dist = np.empty(nsteps)
     for steps in range(nsteps):
         x = tstep.step_rk2(x, dt, b, r, sigma)
-        np.append(dist, np.linalg.norm(x - x0))
+        np.append(dist, np.linalg.norm(x - x_init))
 
     # generate a time values array to plot dist against
-    ts = np.linspace(0, tmax, nsteps)
+    ts = np.linspace(0, dt * nsteps, nsteps)
 
     plt.figure()
     plt.plot(ts, dist)
@@ -32,5 +30,5 @@ def lorenz_points(b, r, sigma, tmax, nsteps, x):  # put dt size instead of nstep
     return None
 
 
-lorenz_eq(8 / 3, 28, 10, 5.0, 500, [x0], save=True)
-lorenz_points(8 / 3, 28, 10, 5.0, 500, x0)
+lorenz_eq(8 / 3, 28, 10, 0.1, 500, [x0], save=True)
+lorenz_points(8 / 3, 28, 10, 0.1, 500, x0)
