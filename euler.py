36acf02f22934680d9f0adc6bd6a872036cc6b6e
Frankie <f.podborski@gmail.com>
2018-02-04 15:23:46 +0000

    minor cosmetic changes

--- euler.py
+++ euler.py
@@ -1,5 +1,6 @@
 # The functions assume that initial condition (IC) is y(0) = y_prev
 
+
 # Standard Euler method
 def euler(f, y_prev, interval, n):
     h = interval / n  # define step size
@@ -8,6 +9,7 @@ def euler(f, y_prev, interval, n):
         y_prev = y_next  # reassign previous estimation of y with next
     print(y_next)
     return None
+
 
 # Modified Euler method
 def modEuler(f, y_prev, interval, n):
@@ -20,6 +22,7 @@ def modEuler(f, y_prev, interval, n):
     print(y_next)
     return None
 
+
 # 4th order Runge-Kutta mathod
 def rk4(f, y_prev, interval, n):
     h = interval / n  # define step size
@@ -28,10 +31,11 @@ def rk4(f, y_prev, interval, n):
         k_2 = h * f(y_prev + 0.5 * k_1)
         k_3 = h * f(y_prev + 0.5 * k_2)
         k_4 = h * f(y_prev + k_3)
-        y_next = y_prev + (k_1 + 2 * k_2 + 2 * k_3 + k_4)/6
+        y_next = y_prev + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
         y_prev = y_next  # reassign previous estimation of y with next
     print(y_next)
     return None
+
 
 euler(lambda y: y, 1.0, 1, 20)
 modEuler(lambda y: y, 1.0, 1, 20)
