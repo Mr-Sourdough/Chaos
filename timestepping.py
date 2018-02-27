5f6ede5d3a2aa09b33f73eaaf6ae4339c4e7c484
Frankie <f.podborski@gmail.com>
2018-02-27 18:33:58 +0000

    cosmetics

--- timestepping.py
+++ timestepping.py
@@ -1,5 +1,6 @@
 """timestepping module"""
 
+
 def calc_dxdt(x, b, r, sigma, linearise=False):
     # Calculates the vector dx / dt for the Lorenz equations
     if linearise is False:
@@ -23,6 +24,7 @@ def step_rk2(x, dt, b, r, sigma, linearise=False):
     x_out = x + k_2
     return x_out
 
+
 def step_rk2_RE(x, dt, b, r, sigma, linearise=False):
     # Runge-Kutta two step method with Richardson Extrapolation
     k_1 = dt * calc_dxdt(x, b, r, sigma, linearise)
@@ -30,10 +32,10 @@ def step_rk2_RE(x, dt, b, r, sigma, linearise=False):
     x_large_step = x + k_2
     k_1 = 0.5 * dt * calc_dxdt(x, b, r, sigma, linearise)
     k_2 = 0.5 * dt * calc_dxdt(x + 0.5 * k_1, b, r, sigma, linearise)
-    x_intermediate_step = x + k_2
-    k_1 = 0.5 * dt * calc_dxdt(x_intermediate_step, b, r, sigma, linearise)
-    k_2 = 0.5 * dt * calc_dxdt(x_intermediate_step + 0.5 * k_1, b, r, sigma, linearise)
-    x_two_small_steps = x_intermediate_step + k_2
+    x_middle_step = x + k_2
+    k_1 = 0.5 * dt * calc_dxdt(x_middle_step, b, r, sigma, linearise)
+    k_2 = 0.5 * dt * calc_dxdt(x_middle_step + k_1 / 2, b, r, sigma, linearise)
+    x_two_small_steps = x_middle_step + k_2
     # Richardson Extrapolation
     x_out = x_large_step + (x_large_step - x_two_small_steps)/3
     return x_out
@@ -48,6 +50,7 @@ def step_rk4(x, dt, b, r, sigma, linearise=False):
     x_out = x + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
     return x_out
 
+
 def step_rk4_RE(x, dt, b, r, sigma, linearise=False):
     # Runge Kutta four step method with Richardson Extrapolation
     k_1 = dt * calc_dxdt(x, b, r, sigma, linearise)
@@ -59,12 +62,12 @@ def step_rk4_RE(x, dt, b, r, sigma, linearise=False):
     k_2 = 0.5 * dt * calc_dxdt(x + 0.5 * k_1, b, r, sigma, linearise)
     k_3 = 0.5 * dt * calc_dxdt(x + 0.5 * k_2, b, r, sigma, linearise)
     k_4 = 0.5 * dt * calc_dxdt(x + k_3, b, r, sigma, linearise)
-    x_intermediate_step = x + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
-    k_1 = 0.5 * dt * calc_dxdt(x_intermediate_step, b, r, sigma, linearise)
-    k_2 = 0.5 * dt * calc_dxdt(x_intermediate_step + 0.5 * k_1, b, r, sigma, linearise)
-    k_3 = 0.5 * dt * calc_dxdt(x_intermediate_step + 0.5 * k_2, b, r, sigma, linearise)
-    k_4 = 0.5 * dt * calc_dxdt(x_intermediate_step + k_3, b, r, sigma, linearise)
-    x_two_small_steps = x_intermediate_step + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
+    x_middle_step = x + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
+    k_1 = 0.5 * dt * calc_dxdt(x_middle_step, b, r, sigma, linearise)
+    k_2 = 0.5 * dt * calc_dxdt(x_middle_step + k_1 / 2, b, r, sigma, linearise)
+    k_3 = 0.5 * dt * calc_dxdt(x_middle_step + k_2 / 2, b, r, sigma, linearise)
+    k_4 = 0.5 * dt * calc_dxdt(x_middle_step + k_3, b, r, sigma, linearise)
+    x_two_small_steps = x_middle_step + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
     # Richardson Extrapolation
     x_out = x_large_step + (x_large_step - x_two_small_steps)/3
     return x_out
