5798584a07cdeaed268e8b377e962cdec7131cb6
Frankie <f.podborski@gmail.com>
2018-02-27 16:45:25 +0000

    changes due to deletion of euler.py file

--- timer.py
+++ timer.py
@@ -1,6 +1,6 @@
 # Timing function to measure speed of code.
 from time import time  # use timeit module instead
-from euler import euler
+import timestepping as tstep
 
 def timer(function, *args, **kwargs):
     t_1 = time()
@@ -8,4 +8,4 @@ def timer(function, *args, **kwargs):
     t_2 = time()
     return t_2 - t_1
 
-print(timer(euler, lambda y: y, 1.0, 1, 8))
+print(timer(tstep.step_rk2, lambda y: y, 1.0, 1, 8))
