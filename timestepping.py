44ca3ea2a8e5d8ca05b2268cc51222854cadf23e
Frankie <f.podborski@gmail.com>
2018-03-04 19:04:30 +0000

    replace separate functions for Richardson Extrapolation with Rich_Extrap

--- timestepping.py
+++ timestepping.py
@@ -19,63 +19,77 @@ def calc_dxdt(x, brsigma, linearise=False):
     return dxdt
 
 
-def step_rk2(x, dt, brsigma, linearise=False):
+def step_rk2(x, h, brsigma, linearise=False):
     # Runge-Kutta second order method
-    k_1 = dt * calc_dxdt(x, brsigma, linearise)
-    k_2 = dt * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
+    k_1 = h * calc_dxdt(x, brsigma, linearise)
+    k_2 = h * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
     x_out = x + k_2
     return x_out
 
 
-def step_rk2_RE(x, dt, brsigma, linearise=False):
+def step_rk2_RE(x, h, brsigma, linearise=False):
     # Runge-Kutta two step method with Richardson Extrapolation
-    k_1 = dt * calc_dxdt(x, brsigma, linearise)
-    k_2 = dt * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
+    k_1 = h * calc_dxdt(x, brsigma, linearise)
+    k_2 = h * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
     x_large_step = x + k_2
-    k_1 = 0.5 * dt * calc_dxdt(x, brsigma, linearise)
-    k_2 = 0.5 * dt * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
+    k_1 = 0.5 * h * calc_dxdt(x, brsigma, linearise)
+    k_2 = 0.5 * h * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
     x_middle_step = x + k_2
-    k_1 = 0.5 * dt * calc_dxdt(x_middle_step, brsigma, linearise)
-    k_2 = 0.5 * dt * calc_dxdt(x_middle_step + k_1 / 2, brsigma, linearise)
+    k_1 = 0.5 * h * calc_dxdt(x_middle_step, brsigma, linearise)
+    k_2 = 0.5 * h * calc_dxdt(x_middle_step + k_1 / 2, brsigma, linearise)
     x_two_small_steps = x_middle_step + k_2
     # Richardson Extrapolation
     x_out = x_large_step + (x_large_step - x_two_small_steps)/3
     return x_out
 
 
-def step_rk4(x, dt, brsigma, linearise=False):
+def step_rk4(x, h, brsigma, linearise=False):
     # Runge-Kutta fourth order method
-    k_1 = dt * calc_dxdt(x, brsigma, linearise)
-    k_2 = dt * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
-    k_3 = dt * calc_dxdt(x + 0.5 * k_2, brsigma, linearise)
-    k_4 = dt * calc_dxdt(x + k_3, brsigma, linearise)
+    k_1 = h * calc_dxdt(x, brsigma, linearise)
+    k_2 = h * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
+    k_3 = h * calc_dxdt(x + 0.5 * k_2, brsigma, linearise)
+    k_4 = h * calc_dxdt(x + k_3, brsigma, linearise)
     x_out = x + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
     return x_out
 
 
-def step_rk4_RE(x, dt, brsigma, linearise=False):
+def step_rk4_RE(x, h, brsigma, linearise=False):
     # Runge Kutta four step method with Richardson Extrapolation
-    k_1 = dt * calc_dxdt(x, brsigma, linearise)
-    k_2 = dt * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
-    k_3 = dt * calc_dxdt(x + 0.5 * k_2, brsigma, linearise)
-    k_4 = dt * calc_dxdt(x + k_3, brsigma, linearise)
+    k_1 = h * calc_dxdt(x, brsigma, linearise)
+    k_2 = h * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
+    k_3 = h * calc_dxdt(x + 0.5 * k_2, brsigma, linearise)
+    k_4 = h * calc_dxdt(x + k_3, brsigma, linearise)
     x_large_step = x + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
-    k_1 = 0.5 * dt * calc_dxdt(x, brsigma, linearise)
-    k_2 = 0.5 * dt * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
-    k_3 = 0.5 * dt * calc_dxdt(x + 0.5 * k_2, brsigma, linearise)
-    k_4 = 0.5 * dt * calc_dxdt(x + k_3, brsigma, linearise)
+    k_1 = 0.5 * h * calc_dxdt(x, brsigma, linearise)
+    k_2 = 0.5 * h * calc_dxdt(x + 0.5 * k_1, brsigma, linearise)
+    k_3 = 0.5 * h * calc_dxdt(x + 0.5 * k_2, brsigma, linearise)
+    k_4 = 0.5 * h * calc_dxdt(x + k_3, brsigma, linearise)
     x_middle_step = x + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
-    k_1 = 0.5 * dt * calc_dxdt(x_middle_step, brsigma, linearise)
-    k_2 = 0.5 * dt * calc_dxdt(x_middle_step + k_1 / 2, brsigma, linearise)
-    k_3 = 0.5 * dt * calc_dxdt(x_middle_step + k_2 / 2, brsigma, linearise)
-    k_4 = 0.5 * dt * calc_dxdt(x_middle_step + k_3, brsigma, linearise)
+    k_1 = 0.5 * h * calc_dxdt(x_middle_step, brsigma, linearise)
+    k_2 = 0.5 * h * calc_dxdt(x_middle_step + k_1 / 2, brsigma, linearise)
+    k_3 = 0.5 * h * calc_dxdt(x_middle_step + k_2 / 2, brsigma, linearise)
+    k_4 = 0.5 * h * calc_dxdt(x_middle_step + k_3, brsigma, linearise)
     x_two_small_steps = x_middle_step + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
     # Richardson Extrapolation
     x_out = x_large_step + (x_large_step - x_two_small_steps)/3
     return x_out
 
 
-def step_midpoint(x, dt, brsigma, linearise=False):
-    k = x + 0.5 * dt * calc_dxdt(x, brsigma, linearise)
-    x_out = x + dt * calc_dxdt(k, brsigma, linearise)
+def step_midpoint(x, h, brsigma, linearise=False):
+    k = x + 0.5 * h * calc_dxdt(x, brsigma, linearise)
+    x_out = x + h * calc_dxdt(k, brsigma, linearise)
     return x_out
+
+
+def Rich_Extrap(x, h, brsigma, method, linearise=False):
+    x_1 = method(x, h, brsigma, linearise)
+    x_temp = method(x, h/2, brsigma, linearise)
+    x_2 = method(x_temp, h/2, brsigma, linearise)
+    x_out = x_1 + (x_1 - x_2)/3
+    return x_out
+
+# def step_size(h, method, tol):
+#     # Takes initial step size h and using local extrapolation extimates h_new
+#     # Only to be used with NON-Extrapolated methods
+#     y_1 = method(x, h, brsigma)
+#     return h_new
