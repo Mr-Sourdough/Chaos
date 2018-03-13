618187fe9385edd5aac6a09027e6618034ea8265
Frankie <f.podborski@gmail.com>
2018-03-13 18:28:02 +0000

    set switch for values of coordinates or plot

--- Lorenz_Equations.py
+++ Lorenz_Equations.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import timestepping as ts
 from mpl_toolkits.mplot3d import Axes3D
+import timestepping as tstep
 
 '''
 brsigma is a tuple with the parameters b, r, and sigma in the Lorenz Equations
@@ -13,7 +13,7 @@ linearise parameter set to True evaluates for the linearised system
 '''
 
 
-def lorenz_eq(brsigma, tmax, h, x_0, linearise=False):
+def lorenz_eq(brsigma, tmax, h, x_0, method, linearise=False, coord=True):
     x = x_0
     # initialise xs, ys and zs
     xs = np.array(x_0[0])
@@ -28,22 +28,29 @@ def lorenz_eq(brsigma, tmax, h, x_0, linearise=False):
     while t <= tmax:
         # times.append(t)
         # hs.append(h)
-        x = ts.step_rk4(x, h, brsigma, linearise)
+        x = method(x, h, brsigma, linearise)
         xs = np.append(xs, x[0])
         ys = np.append(ys, x[1])
         zs = np.append(zs, x[2])
         # here put function for generating h
-        # h = ts.step_size(x, h, brsigma, ts.step_rk4, 4, 10**-4)
+        # h = ts.step_size(x, h, brsigma, method, 4, 10**-4)
         t += h
+    if coord is True:
+        return xs, ys, zs
+    else:
+        # create 3d plot
+        fig = plt.figure("Lorenz System")
+        ax = Axes3D(fig)
+        ax.set_title("Lorenz System")
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        ax.scatter(x_0[0], x_0[1], x_0[2])
+        ax.plot(xs, ys, zs)
+        # print(times, hs, len(times), sep='\n')
+        return fig
 
-    # create 3d plot
-    fig = plt.figure("Lorenz System")
-    ax = Axes3D(fig)
-    ax.set_title("Lorenz System")
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
-    ax.scatter(x_0[0], x_0[1], x_0[2])
-    ax.plot(xs, ys, zs)
-    # print(times, hs, len(times), sep='\n')
-    return fig
+
+lorenz_eq((1, 4, 1), 1.0, 0.01, np.array([1, -1, 2]),
+          tstep.step_rk2, coord=False)
+plt.show()
--- linear_soln.py
+++ linear_soln.py
@@ -2,20 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from math import exp
-from Lorenz_Equations import lorenz_eq
 
 """
 -Need to cater for non-constant h
 -Utilise fixed_point_analysis.py code for error calculation
 """
 
-brsigma = (1, 4, 1)
-x_0 = np.array([1, -1, 2])
-h = 0.01
-tmax = 1.0
 
-
-def exact_lin_soln(brsigma, tmax, h, x_0):
+def exact_lin_soln(brsigma, tmax, h, x_0, coord=True):
     # Jacobian matrix calculated at origin
     Jacobian_origin = np.array([[-brsigma[2], brsigma[2], 0],
                                 [brsigma[1], -1, 0],
@@ -30,7 +24,7 @@ def exact_lin_soln(brsigma, tmax, h, x_0):
     ys = np.array([x_0[1]])
     zs = np.array([x_0[2]])
 
-    for t in np.arange(0+h, 1.0, h):
+    for t in np.arange(0+h, tmax+h, h):
         x = x_0 * 0
         for i in range(3):
             x = np.add(x, C[i] * vec[:, i] * exp(val[i]*t))
@@ -38,13 +32,18 @@ def exact_lin_soln(brsigma, tmax, h, x_0):
         ys = np.append(ys, x[1])
         zs = np.append(zs, x[2])
 
-    fig = plt.figure("Exact Linear Solution")
-    ax = Axes3D(fig)
-    ax.scatter(x_0[0], x_0[1], x_0[2])
-    ax.plot(xs, ys, zs)
-    return fig
+    if coord is True:
+        return xs, ys, zs
+    else:
+        # creates 3d plot
+        fig = plt.figure("Exact Linear Solution")
+        ax = Axes3D(fig)
+        ax.set_title("{}".format(x_0))
+        ax.scatter(x_0[0], x_0[1], x_0[2])
+        ax.scatter(0, 0, 0)
+        ax.plot(xs, ys, zs)
+        return fig
 
 
-exact_lin_soln(brsigma, tmax, h, x_0)
-lorenz_eq(brsigma, tmax, h, [x_0], linearise=True)
+exact_lin_soln((2, 7, 1), 1.0, 0.01, np.array([-1, 1, 0]), coord=False)
 plt.show()
