44cd125d583c5f01befc5b592ecf89e863968136
Frankie <f.podborski@gmail.com>
2018-03-17 13:48:54 +0000

    now simply a module

--- error_analysis.py
+++ error_analysis.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from plotting_3d import exact_lin_soln, lorenz_eq
-import timestepping as tstep
 
 """
-- Cannot use extrapolation to measure accuracy
 - Not appropriate for varying h yet
 - Only gives plot, not suitable for creating tables
 """
@@ -20,8 +18,7 @@ def method_error(brsigma, tmax, h, x_0, method):
 
     xs, ys, zs = lorenz_eq(brsigma, tmax, h, x_0, method, linearise=True)
     exact_xs, exact_ys, exact_zs = exact_lin_soln(brsigma, tmax, h, x_0)
-
-    for i in range(len(xs)):
+    for i in range(len(exact_xs)):
         method_vec = np.array([xs[i], ys[i], zs[i]])
         exact_vec = np.array([exact_xs[i], exact_ys[i], exact_zs[i]])
         error = np.append(error, np.linalg.norm(method_vec - exact_vec))
@@ -34,8 +31,3 @@ def method_error(brsigma, tmax, h, x_0, method):
     plt.ylabel('distance from exact solution')
     plt.semilogy(ts, error)
     return fig
-
-
-method_error((1, 4, 1), 2.0, 0.01, np.array([0, -1, 2]), tstep.step_rk4)
-method_error((1, 4, 1), 2.0, 0.01, np.array([0, -1, 2]), tstep.step_rk2)
-plt.show()
