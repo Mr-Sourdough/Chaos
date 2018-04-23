6e730cda54db5d451e5ff7713d50a01a3ea3b7f3
Frank Podborski <f.podborski@gmail.com>
2018-04-23 23:50:19 +0100

    creates plot data for animation and stores tuple of coordinate lists as .pickle

---
+++ plot_data.py
@@ -0,0 +1,12 @@
+from plotting_3d import lorenz_eq
+import numpy as np
+from timestepping import RE_rk4
+import pickle
+
+xs, ys, zs = lorenz_eq((8/3, 28, 10), 10.0, 0.01, np.array([1, 1, 1]), RE_rk4)
+
+with open("plot_data.pickle", 'bw') as data:
+    pickle.dump((list(xs), list(ys), list(zs)), file=data)
+
+with open("plot_data.pickle", 'br') as data:
+    coords = pickle.load(data)
