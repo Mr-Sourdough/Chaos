0f97f362f57f8962243bfaed5515ed6a393a793a
Frank Podborski <f.podborski@gmail.com>
2018-04-24 01:01:16 +0100

    working animation

--- Animator.py
+++ Animator.py
@@ -1,11 +1,38 @@
 from plotting_3d import draw_axes, lorenz_eq
 from timestepping import RE_rk4
 from matplotlib import animation
+from matplotlib import pyplot as plt
+import pickle
 
-ax = draw_axes()
-brsigma = (8/3, 28, 10)
+with open("plot_data.pickle", 'br') as data:
+    coords = pickle.load(data)
 
-line, = lorenz_eq(brsigma, 10.0, 0.01, RE_rk4, ax)
+xs, ys, zs = coords
+ax, final_plot = draw_axes(title="Lorenz System")
+ax.set_xlim(left=-20, right=15)
+ax.set_ylim(bottom=-30, top=30)
+ax.set_zlim(bottom=0, top=40)
+line, = ax.plot([], [], [])
 
-FinalPlot = draw_axes(title="Lorenz System")
+def init():
+    line.set_data([], [])
+    line.set_3d_properties([])
+    return line,
 
+def animate(i):
+    # two time steps per frame
+    i *= 4
+    # for each i get the values of the integration
+    x = xs[:i]
+    y = ys[:i]
+    z = zs[:i]
+    # plot the values as the line
+    line.set_data(x, y)
+    line.set_3d_properties(z)
+    final_plot.canvas.draw()
+    return line,
+
+anim = animation.FuncAnimation(final_plot, animate, init_func=init,
+                                frames=750, interval=20, blit=True)
+
+plt.show()
